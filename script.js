c777bb611f817476d1da4d37b8dd42425e2a50ed
Karl Lankester <kzdrenka@gmail.com>
2022-07-20 15:40:27 +0100

    v1.0.1 added clear and copy cuttons, token is now stored in local storage until cleared, token is now persistant in popup

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
-async function decode() {
-  var token = document.getElementById("token").value;
-  var decoded = parseJwt(token);
-  document.getElementById("decoded").textContent = JSON.stringify(
-    decoded,
-    null,
-    2
+var stack = [];
+
+let token = document.getElementById("token");
+let decoded = document.getElementById("decoded");
+
+let copyButton = document.getElementById("copy");
+let clearButton = document.getElementById("clear");
+let decodeButton = document.getElementById("decode");
+
+function get_data() {
+  // Use default value color = 'red' and likesColor = true.
+  console.info("loading from storage");
+  chrome.storage.sync.get(
+    {
+      stack: "data",
+    },
+    function (items) {
+      if (Array.isArray(items.stack)) {
+        token.value = items.stack[0].token;
+        decoded.textContent = items.stack[0].result;
+        toggleButtons();
+        console.log("got data");
+      }
+    }
   );
+}
+
+function storeToken(stack) {
+  chrome.storage.sync.set(
+    {
+      stack: stack,
+    },
+    function () {
+      console.info("storing");
+    }
+  );
+}
+
+function decodeJWT() {
+  stack.push({
+    token: token.value,
+    result: (decoded.textContent = JSON.stringify(
+      parseJwt(token.value),
+      null,
+      2
+    )),
+  });
+  storeToken(stack);
+  toggleButtons();
+}
+
+function toggleButtons() {
+  let display = decoded.textContent.length > 0 ? "block" : "none";
+  copyButton.style.display = display;
+  clearButton.style.display = display;
 }
 
 function parseJwt(token) {
@@ -24,4 +71,38 @@ function parseJwt(token) {
   return JSON.parse(jsonPayload);
 }
 
-document.getElementById("go").addEventListener("click", decode);
+function copyText() {
+  navigator.clipboard.writeText(decoded.textContent);
+
+  copyButton.textContent = "Copied!";
+  setTimeout(function () {
+    copyButton.textContent = "Copy";
+  }, 750);
+}
+
+function options() {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL("options.html"));
+  }
+}
+
+function clear() {
+  decoded.textContent = "";
+  token.value = "";
+  chrome.storage.sync.clear();
+  toggleButtons();
+}
+
+//hide buttons until we have data
+copyButton.style.display = "none";
+clearButton.style.display = "none";
+
+copyButton.addEventListener("click", copyText);
+decodeButton.addEventListener("click", decodeJWT);
+clearButton.addEventListener("click", clear);
+
+document.addEventListener("DOMContentLoaded", get_data);
+
+toggleButtons();
