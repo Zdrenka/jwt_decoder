d2708d725f2c62f68056df33120af0ba283e004b
Karl Lankester-Carthy <2591040+Zdrenka@users.noreply.github.com>
2024-07-25 17:15:36 +0100

    feat: addded dark mode

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-var stack = [];
+const stack = [];
 
-let token = document.getElementById("token");
-let container = document.getElementById("container");
-let header = document.getElementById("header");
-let payload = document.getElementById("payload");
+const token = document.getElementById("token");
+const container = document.getElementById("container");
+const header = document.getElementById("header");
+const payload = document.getElementById("payload");
 
-let copyButton = document.getElementById("copy");
-let copyPayloadButton = document.getElementById("copy_payload");
-let copyHeaderButton = document.getElementById("copy_header");
-let clearButton = document.getElementById("clear");
-let decodeButton = document.getElementById("decode");
+const copyButton = document.getElementById("copy");
+const copyPayloadButton = document.getElementById("copy_payload");
+const copyHeaderButton = document.getElementById("copy_header");
+const clearButton = document.getElementById("clear");
+const decodeButton = document.getElementById("decode");
+const toggleThemeButton = document.getElementById("toggle-theme");
+
+const dark = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="svg-icon bi bi-lightbulb" viewBox="0 0 16 16">
+  <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1/>"
+</svg>`
+
+const light = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="svg-icon bi bi-lightbulb" viewBox="0 0 16 16">
+  <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5"
+    fill="yellow" stroke="#dee2e6" stroke-width="0.4"/>
+</svg>`
 
 function get_data() {
   // Use default value color = 'red' and likesColor = true.
@@ -28,6 +38,23 @@ function get_data() {
       }
     }
   );
+
+    chrome.storage.sync.get('theme', (data) => {
+    if (data.theme) {
+      document.body.classList.add(data.theme);
+      updateSVG(data.theme);
+    }
+  });
+
+  // Toggle theme
+  toggleThemeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    const theme = document.body.classList.contains("dark-mode") ? "dark-mode" : "light-mode";
+    
+    // Save theme to storage
+    chrome.storage.sync.set({ theme });
+    updateSVG(theme);
+  });
 }
 
 function storeToken(stack) {
@@ -123,6 +150,14 @@ function options() {
     chrome.runtime.openOptionsPage();
   } else {
     window.open(chrome.runtime.getURL("options.html"));
+  }
+}
+
+function updateSVG(theme) {
+  if (theme === "dark-mode") {
+    toggleThemeButton.innerHTML = dark;
+  } else {
+    toggleThemeButton.innerHTML = light;
   }
 }
 
